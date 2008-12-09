b15b683d682131abf6ae4c29180cdcf8fb78f4ec
Jesse Wolfe <jes5199@electra.jes5199.com>
2008-12-09 14:44:24 -0800

    i think this is sane loop detection

--- sha.rb
+++ sha.rb
@@ -1,16 +1,17 @@
 require 'sha1'
 require 'rubygems'
 require 'bloomfilter'
-filter = BloomFilter.new(100)
+filter = BloomFilter.new(100000000)
 sha    = SHA1.digest('')
 i = 0
 
 loop do
   if filter.include?(sha)
-    raise "collision at #{i}"
+    puts "collision at #{i}"
   end
 
-  filter.add(sha)
+  filter.add(sha) if   i % 10000 == 0
+  p i             if i % 1000000 == 0
   sha  = SHA1.digest(sha)
   i+=1
 end

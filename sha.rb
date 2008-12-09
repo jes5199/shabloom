6695d76e683b6017ef7e9d3a6748774a55564a55
Jesse Wolfe <jes5199@electra.jes5199.com>
2008-12-09 13:57:17 -0800

    use off-the-shelf bloomfilter

--- sha.rb
+++ sha.rb
@@ -1,57 +1,16 @@
 require 'sha1'
-require 'md5'
-
-COUNT = 12
-start = SHA1.digest('')
-
-bloom = "\x0" * ( (start.length) + (16 * COUNT) )
-
-def bloom_add(filter, item)
-  result   = ""
-  filterIt = filter.bytes.each
-  itemIt   = item.bytes.each
-
-  begin
-    loop do
-      f = filterIt.next
-      i = (itemIt.next rescue 0)
-      result += (f | i).chr
-    end
-  rescue StopIteration
-  end
-  return result
-end
-
-def bloom_test(filter, item)
-
-  filterIt = filter.bytes.each
-  itemIt   = item.bytes.each
-
-  begin
-    loop do
-      f = filterIt.next
-      i = (itemIt.next rescue 0)
-      return false if ( (~f) & i ) != 0
-    end
-  rescue StopIteration
-  end
-  return true
-end
-
-sha = start
+require 'rubygems'
+require 'bloomfilter'
+filter = BloomFilter.new(100)
+sha    = SHA1.digest('')
 i = 0
 
 loop do
-  bloomtest = sha
-  COUNT.times do
-    bloomtest += MD5.digest(bloomtest)
-  end
-
-  if bloom_test(bloom, bloomtest)
+  if filter.include?(sha)
     raise "collision at #{i}"
   end
 
-  bloom = bloom_add(bloom, bloomtest)
+  filter.add(sha)
   sha  = SHA1.digest(sha)
   i+=1
 end

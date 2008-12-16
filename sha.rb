cb42f0f0f038a95476eeb419a2109e2dd281e3d3
Jesse Wolfe <jes5199@electra.jes5199.com>
2008-12-15 19:30:14 -0800

    auto testing for false positives, first attempt at checkpointing

--- sha.rb
+++ sha.rb
@@ -1,18 +1,52 @@
 require 'sha1'
 require 'rubygems'
 require 'bloomfilter'
-filter = BloomFilter.new(100000000)
-sha    = SHA1.digest('')
+require 'pstore'
+
+#filter = BloomFilter.new(100000000)
+starter = ''
+filter = BloomFilter.new(5000000)
+sha    = SHA1.digest(starter)
 i = 0
+interesting = Hash.new(0)
+
+period = 100000
+
+hit_size = 1000
+old_hits = BloomFilter.new(hit_size)
+hits     = BloomFilter.new(hit_size)
 
 loop do
   if filter.include?(sha)
-    puts "collision at #{i}"
+    print "collision at #{i}"
+    if old_hits.include?( (i - period).to_s )
+      print " LOOKS INTERESTING"
+      puts ""
+      interesting[ i % period ] += 1
+      if interesting[ i % period ] > 3
+        raise "LOOKS REALLY INTERESTING"
+      end
+    end
+    hits.add(i.to_s)
+    puts ""
   end
 
-  filter.add(sha) if   i % 10000 == 0
-  p i             if i % 1000000 == 0
-  sha  = SHA1.digest(sha)
+  if i % period == 0
+    old_hits = hits
+    hits     = BloomFilter.new(hit_size)
+
+    filter.add(sha) 
+    p i
+    if i % (period * 10)
+      PStore.new('checkpoint').transaction do | s |
+        s[:starter] = starter
+        s[:sha]     = sha
+        s[:i]       = i
+      end
+    end
+  end
+
+  sha  = SHA1.digest(sha)[0..7]
   i+=1
 end
 
